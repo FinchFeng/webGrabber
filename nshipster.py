5342998f016a0a0e834cb97e89e6837c65554960
FinchFeng <329650437@qq.com>
2019-04-24 21:13:19 +0800

    nshipster完成

--- nshipster.py
+++ nshipster.py
@@ -4,16 +4,33 @@ from dataModel import dataModel
 
 def getNshipster():
     dataArray = []
-    url = "https://nshipster.com/"
+    url = "https://nshipster.com"
     result = requests.get(url)
     soup = BeautifulSoup(result.text,"html.parser")
     div_set = soup.find_all("a",class_="title")
+    titleArray = []
+    urlArray = []
     for div in div_set:
         title = div.get_text()
+        theURL = url + div.get("href")
+        urlArray.append(theURL)
         # eachUrl = "https://www.hackingwithswift.com" + div.get("href")
         # imageUrl = "https://www.hackingwithswift.com" + div.find("img").get("src")
         # dataArray.append(dataModel(title,eachUrl,imageUrl,"swift"))
-        print(title)
+        titleArray.append(title)
+    li_set = soup.find_all("li")
+    textArray = []
+    for li in li_set:
+        p = li.find("p")
+        if p != None :
+            text = p.getText()
+            textArray.append(text)
+
+    # print(titleArray)
+    # print(urlArray)
+    # print(textArray)
+    for index in range(0,len(titleArray)):
+        dataArray.append(dataModel(titleArray[index]+textArray[index],urlArray[index],"nil","swift"))
     return dataArray
 
 if __name__ == "__main__":
