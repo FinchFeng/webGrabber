a4111542883902fddfa0c15025bfdafa33359cf7
gaays <shujiapeng@outlook.com>
2019-05-14 21:14:01 +0800

    科技测评网站爬完

---
+++ TechnologyArticles/TechnicalCourse/appso.py
@@ -0,0 +1,34 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+from dataModel import dataModel
+
+def getPcbeta_tech():
+    url = "https://www.ifanr.com/app"
+    result = requests.get(url)
+    dataArray = []
+    imageArray = []
+    i = 0
+    soup = BeautifulSoup(result.text, "html.parser")
+    div_set = soup.find_all("div", class_="article-info")
+    img_set = soup.find_all("a", class_="article-link cover-block")
+    for img in img_set:
+        txt = str(img['style'])
+        # 正则表达式
+        p1 = r'https://.*260'
+        # 将正则表达式编译成Pattern对象
+        pattern = re.compile(p1)
+        imageUrl = str(pattern.findall(txt))
+        imageArray.append(imageUrl)
+    for div in div_set:
+        title = div.find("h3").find("a").get_text()
+        eachUrl = div.find("h3").find("a").get("href")
+        dataArray.append(dataModel(title,eachUrl,imageArray[i],"course"))
+        i+=i
+    return dataArray
+
+if __name__ == "__main__":
+    array = getPcbeta_tech()
+    for item in array:
+        item.printIt()
---
+++ TechnologyArticles/TechnologyArticles/sspaiArticles.py
@@ -0,0 +1,19 @@
+import requests
+from dataModel import dataModel
+
+def getPcbeta_tech():
+    url = "https://sspai.com/api/v1/articles?offset=0&limit=20&has_tag=1&tag=%E7%83%AD%E9%97%A8%E6%96%87%E7%AB%A0&include_total=false&type=recommend_to_home"
+    list = requests.get(url).json()
+    dataArray = []
+    llist = list['list']
+    for i in range(len(llist)):
+        title = llist[i]['title']
+        eachUrl = "https://sspai.com/post/" + str(llist[i]['id'])
+        imageUrl = "https://cdn.sspai.com/" + llist[i]['banner']
+        dataArray.append(dataModel(title,eachUrl,imageUrl,"techArticles"))
+    return dataArray
+
+if __name__ == "__main__":
+    array = getPcbeta_tech()
+    for item in array:
+        item.printIt()
