477a7f83977634e75153e2b9aed2ce8e3992cbdf
gaays <shujiapeng@outlook.com>
2019-05-14 20:39:05 +0800

    科技网站爬虫（按分类）

---
+++ TechnologyArticles/Computer/pcbeta.py
@@ -0,0 +1,28 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+from dataModel import dataModel
+
+def getPcbeta():
+    url = "http://www.pcbeta.com/news/"
+    headers = ("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45")
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+    data = opener.open(url).read().decode('gbk')
+    dataArray = []
+    soup = BeautifulSoup(data,"html.parser")
+    div_set = soup.find_all("a", class_="thumb")
+    # print(div_set)
+    for div in div_set:
+        if(div.find("img").get("title")):
+            title = div.find("img").get("title")
+            eachUrl = div.get("href")
+            imageUrl = div.find("img").get("src")
+            dataArray.append(dataModel(title,eachUrl,imageUrl,"computer"))
+
+    return dataArray
+
+if __name__ == "__main__":
+    array = getPcbeta()
+    for item in array:
+        item.printIt()
---
+++ TechnologyArticles/ResourceSharing/sspaiArticles.py
@@ -0,0 +1,19 @@
+import requests
+from dataModel import dataModel
+
+def getPcbeta_tech():
+    url = "https://sspai.com/api/v1/articles?offset=0&limit=20&has_tag=1&tag=%E5%BA%94%E7%94%A8%E6%8E%A8%E8%8D%90&include_total=false&type=recommend_to_home"
+    list = requests.get(url).json()
+    dataArray = []
+    llist = list['list']
+    for i in range(len(llist)):
+        title = llist[i]['title']
+        eachUrl = "https://sspai.com/post/" + str(llist[i]['id'])
+        imageUrl = "https://cdn.sspai.com/" + llist[i]['banner']
+        dataArray.append(dataModel(title,eachUrl,imageUrl,"resource"))
+    return dataArray
+
+if __name__ == "__main__":
+    array = getPcbeta_tech()
+    for item in array:
+        item.printIt()
